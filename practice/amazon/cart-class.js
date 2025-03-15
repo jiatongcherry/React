b48acc6fe6672325d11e8d121d49ca3827bfeddf
jiatongcherry <jiatongluna@icloud.com>
2025-03-15 18:40:54 +0800

    Add files via upload

---
+++ practice/amazon/cart-class.js
@@ -0,0 +1,87 @@
+class Cart {
+  cartItems = undefined;
+  localStorageKey = undefined;
+  loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    
+      if (!this.cartItems) {
+        this.cartItems = [
+          {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: '1'
+          },
+          {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionId: '2'
+          }];
+      }
+    }
+    saveToStorage() {
+      localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    }
+    addToCart(productId) {
+      let matchingItem;
+    
+      this.cartItems.forEach((item) => {
+        if (item.productId === productId) {
+          matchingItem = item;
+        }
+      });
+    
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      }
+      else {
+        this.cartItems.push({
+          productId: productId,
+          quantity: 1,
+          deliveryOptionId: '1'
+        });
+      }
+    
+      this.saveToStorage();
+    }
+    removeFromCart(productId) {
+      const newCart = [];
+    
+      this.cartItems.forEach((item) => {
+        if (item.productId !== productId) {
+          newCart.push(item);
+        }
+      });
+    
+      this.cartItems = newCart;
+    
+      this.saveToStorage();
+    }
+    updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+    
+      this.cartItems.forEach((item) => {
+        if (item.productId === productId) {
+          matchingItem = item;
+        }
+      });
+    
+    
+      matchingItem.deliveryOptionId = deliveryOptionId;
+    
+      this.saveToStorage();
+    }
+}
+
+
+
+const cart = new Cart();
+const businessCart = new Cart();
+
+cart.localStorageKey = 'cart-oop';
+businessCart.localStorageKey = 'cart-business';
+
+cart.loadFromStorage(); 
+
+cart.addToCart('8c9c52b5-5a19-4bcb-a5d1-158a74287c53');
+console.log(cart);
+console.log(businessCart)
